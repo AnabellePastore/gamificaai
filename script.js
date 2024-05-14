d4691bff267e77443b3641e939b4596a161632ac
AnabellePastore <42332170812@info.local>
2024-05-14 17:21:12 -0300

    feat: carrossel feito

--- script.js
+++ script.js
@@ -25,3 +25,56 @@ function abrirFecharMenu(){
        
 
 }
+
+//função carrosel
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+let slideAtual = 0
+
+let numeroSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+    //remove o slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    //numeroSlides = 3
+    //numero Slides -1 -> 2
+    //estou no ultimo? 2
+
+
+        if(slideAtual < numeroSlides -1){
+            slideAtual ++
+        }else{
+            slideAtual = 0
+        }
+
+
+
+    //rendiriza o slideAtual
+    banner.classList.add(slides[slideAtual])
+
+}
+
+const mostrarSlideAnterior = () => {
+    banner.classList.remove(slides[slideAtual])
+
+    if(slideAtual > 0){
+        slideAtual--
+    } else{
+        slideAtual = numeroSlides - 1
+    }
+
+   
+
+    banner.classList.add(slides[slideAtual])
+
+
+}
